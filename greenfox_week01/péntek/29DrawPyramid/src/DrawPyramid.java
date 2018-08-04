8440b5c599296ef48a2fd0717cd7236dee7226d4
Szabó Zsófia <szabozsofiaeva@gmail.com>
2018-08-04 19:31:45 +0200

    feat 29 feladat kitörölve, új 29 feladat hozzáadva

--- greenfox_week01/péntek/29DrawPyramid/src/DrawPyramid.java
+++ greenfox_week01/péntek/29DrawPyramid/src/DrawPyramid.java
@@ -2,33 +2,27 @@ import java.util.Scanner;
 public class DrawPyramid {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int num = scanner.nextInt();
-        int a = 1;
-        int b = 1;
-        for (a=1; a <= num; a++){
+        int x = 1;
+        int z = 1;
+        int w = 1;
+        int y = scanner.nextInt();
+        int g =1;
 
-            for ( b = 1; b <= a; b++){
+        for (z = 1; z <= y++; z+=2) {
+
+            for (g = 1; g <= y-z; g++) {
                 System.out.print(" ");
+            }
+            for (x = 1; x <= z; x++) {
                 System.out.print("*");
-                System.out.println(" ");
-
-        }System.out.println();
+            }
+            System.out.println();
 
         }
-
     }
 }
 
 
 
 
-// Write a program that reads a number from the standard input, then draws a
-// pyramid like this:
-//
-//
-//    *
-//   ***
-//  *****
-// *******
-//
-// The pyramid should have as many lines as the number was
+
