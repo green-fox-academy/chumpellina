374113b9f2cc50556dadcef79587eb7fd85e53b9
Szabó Zsófia <szabozsofiaeva@gmail.com>
2018-08-06 18:15:04 +0200

    feat added 10ChangeElement

---
+++ greenfox_week02/hétfő/10ChangeElement/src/ChangeElement.java
@@ -0,0 +1,17 @@
+public class ChangeElement {
+    public static void main(String[] args) {
+        int [] s = {1,2,3,8,5,6};
+        System.out.println(cserelo(s));
+
+    }
+    public static int cserelo (int [] s) {
+        return s[3]=4;
+    }
+}
+
+
+
+/*# - Create an array variable named `s`
+        #   with the following content: `[1, 2, 3, 8, 5, 6]`
+        # - Change the 8 to 4
+        # - Print the fourth element */
