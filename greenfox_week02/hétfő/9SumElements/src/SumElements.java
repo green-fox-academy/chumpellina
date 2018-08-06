7a1483f75849a3ffd193b4cbf432177e13d3c000
Szabó Zsófia <szabozsofiaeva@gmail.com>
2018-08-06 17:49:38 +0200

    feat added task 9

---
+++ greenfox_week02/hétfő/9SumElements/src/SumElements.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+public class SumElements {
+    public static void main(String[] args) {
+        int [] r ={54, 23, 66, 12};
+        System.out.println(ttt (r));
+
+    }
+    public static int ttt (int [] r) {
+       return r[1]+= r[2];
+    }
+}
