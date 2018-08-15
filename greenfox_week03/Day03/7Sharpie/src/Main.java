0ccfed55f538d68da1562cc6dc72318454e9736d
Szabó Zsófia <szabozsofiaeva@gmail.com>
2018-08-15 21:09:54 +0200

    feat added task7 to week03/day3

---
+++ greenfox_week03/Day03/7Sharpie/src/Main.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String... args) {
+        Sharpie sharpie1 = new Sharpie("brown", 10, 100 );
+        System.out.println(sharpie1.toString());
+        System.out.println(sharpie1.toString());
+    }
+}
