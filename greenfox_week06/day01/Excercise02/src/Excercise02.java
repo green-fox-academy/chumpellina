9d38e209e31cb0be98cd3949d3d860ab22937283
Szabó Zsófia <szabozsofiaeva@gmail.com>
2018-09-10 14:49:58 +0200

    added exercise02 to week06/day01

--- greenfox_week06/day01/Excercise02/src/Excercise02.java
+++ greenfox_week06/day01/Excercise02/src/Excercise02.java
@@ -1,2 +1,21 @@
+import javax.sound.sampled.Line;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+
+import static jdk.nashorn.internal.objects.NativeArray.map;
+
 public class Excercise02 {
+    public static void main(String[] args) {
+        /// Write a Stream Expression to get the average value of the odd numbers from the following array:
+
+        ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14));
+
+        numbers.stream()
+                .mapToInt(n -> n)
+                .filter(n -> n % 2 == 1)
+                .average()
+                .ifPresent(avg -> System.out.println(avg));
+    }
 }
